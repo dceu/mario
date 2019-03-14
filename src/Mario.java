b13c686d58dd51c90f9e72ef628313ac5c364ed5
dceu <dceu00@gmail.com>
2019-03-14 02:11:46 -0400

    pset 1: complete

--- src/Mario.java
+++ src/Mario.java
@@ -1,13 +1,31 @@
 
 class Mario {
-public static void main(String[] args) {
-	PyramidHeight.prompt();
-	int height = PyramidHeight.scanHeight();
-	while (PyramidHeight.validateHeight(height) == false){
-		PyramidHeight.enforce();
+	public static void prompt() {
 		PyramidHeight.prompt();
-		height = PyramidHeight.scanHeight();
 	}
+
+	public static int validateLoop() {
+		PyramidHeight.scanHeight();
+			int height;
+			while (PyramidHeight.validateHeight(height) == false){
+			PyramidHeight.enforce();
+			PyramidHeight.prompt();
+			 height = PyramidHeight.scanHeight();
+			}
+			return height;
+
+	}
+	
+	public static void drawPyramid(int h){
+		int height = h;
+		int length = h;
+
+	}
+	
+public static void main(String[] args) {
+	prompt();
+	int height = validateLoop();
 	System.out.println("Your pyramid height is: " + height);	
-}
+	drawPyramid(height);
+	}
 }
